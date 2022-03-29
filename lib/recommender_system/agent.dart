62cbd45ad61953d419a59770bdf87fcba1eb03bd
Faith <faith_new@hotmail.com>
2022-03-29 18:50:41 +0100

    Added code for recommender agent

---
+++ lib/recommender_system/agent.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+class Agent {
+  final _modelFile = "model.tflite";
+
+  late Interpreter _interpreter;
+
+  Agent() {
+    _loadModel();
+  }
+
+  void _loadModel() async {
+    _interpreter = await Interpreter.fromAsset(_modelFile);
+    assert(_interpreter != null);
+  }
+
+  List predict(String itemID) {
+    var input = itemID;
+    var output = List.filled(3, "0");
+
+    // Inference
+    _interpreter.run(input, output);
+
+    debugPrint(output.toString());
+
+    return output;
+  }
+}
